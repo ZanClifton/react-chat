abdff9a101f81ed1a6c2476d2bc6026de133609e
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-07-28 09:08:45 +0100

    both servers running

--- server/index.js
+++ server/index.js
@@ -3,21 +3,22 @@ const app = express();
 const http = require("http");
 const server = http.Server(app);
 const { Server } = require("socket.io");
-const io = new Server(server, { cors: { origin: "http://localhost:3000" } });
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET, POST"],
+  },
+});
 const cors = require("cors");
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
-
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected");
-//   });
-// });
 
 // io.on("connection", (socket) => {
 //   socket.on("chat message", (msg) => {
@@ -35,6 +36,6 @@ app.get("/", (req, res) => {
 //   });
 // });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(3001, () => {
+  console.log("listening on *:3001");
 });
